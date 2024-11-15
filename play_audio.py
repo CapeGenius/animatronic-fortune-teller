0d9b7770bdb56457390fd91140ed06552197e14c
CapeGenius <datanteater@gmail.com>
2024-11-15 16:01:24 -0500

    created an audio python script based on the serial port

---
+++ play_audio.py
@@ -0,0 +1,19 @@
+from serial import Serial
+from pygame import mixer  # Load the popular external library
+
+arduinoComPort = "\\.\COM6"
+baudRate = 9600
+serialPort = Serial(arduinoComPort, baudRate, timeout=5)
+
+while True:
+    serialPort.flushInput()
+    lineOfData = serialPort.readline().decode()
+
+    if lineOfData  == 1:
+        mixer.init()
+        mixer.music.load("recording.mp3")
+        mixer.music.play()
+    else:
+        mixer.init()
+        mixer.music.stop()
+        
