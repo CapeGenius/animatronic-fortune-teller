0ddb0a356fa97a0ee6dbd88614be8bb162d6dfc5
Rohan Bendapudi <datanteater@gmail.com>
2024-12-17 21:19:25 -0500

    updated the python and arduino code

--- button_press/button_press.ino
+++ button_press/button_press.ino
@@ -1,51 +1,38 @@
 // the PWM pin the LED is attached to
 #include <Button.h>
 #include <Servo.h>
-#include <Adafruit_PWMServoDriver.h>
 
-Button button1(2);
+Button button1(0);
 
-Servo myservo;  // create Servo object to control a servo
-Servo myservo2;  // create Servo object to control a servo
-Servo myservo_tilt;
-Servo myservo_pan;
-Servo torsoServo;
+Servo leftArmServo,torsoServo, tiltServo, panServo, rightArmServo;  // Create a Servo object
 
-// Adafruit PWM Servo Driver
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+// positions of the servos
+int pos_tilt = 60;
+int pos_arm = 60;
 
-// Constants for Adafruit Motor Shield
-#define SERVO_MIN  150  // Minimum pulse length count
-#define SERVO_MAX  600  // Maximum pulse length count
-int adafruitServoPos = 90; // Initial position for the Adafruit Motor Shield servo
-int pos_mov_adafruit = 1;
+// servo movements
+int pos_tilt_mov = 1;  // Reduced step size for slower tilt servo movement
+int pos_arm_mov = 1;
 
-int pos = 10;    // variable to store the servo position
-int pos_pan = 80;
-int pos_torso = 91;
-int pos_mov = 1;
-int pos_mov_tilt = 1;
-int pos_mov_torso = 1;
-int buttonState = 0;
+// servos arm min and max
+int pos_arm_min = 10;
+int pos_arm_max = 150;
+
+// servos tilt min and max
+int pos_tilt_min = 60;
+int pos_tilt_max = 70;
+
+
 int number = 0;
-int lastButtonState = 0;
-int SW1 = 2;
-int on_off;
+int on_off = 0;
 
 // the setup routine runs once when you press reset:
 void setup() {
 
-
-  // declare pin 9 to be an output:
-  myservo.attach(3);  // attaches the servo on pin 9 to the Servo object
-  myservo2.attach(9);  // attaches the servo on pin 9 to the Servo object  
-  myservo_tilt.attach(6);  // attaches the servo on pin 9 to the Servo object  
-  myservo_pan.attach(5);
-  pinMode(SW1, INPUT_PULLUP);
+  tiltServo.attach(10);
+  rightArmServo.attach(3);
   Serial.begin(9600);
 
-  pwm.begin();
-  pwm.setPWMFreq(50);  // Set frequency to 50 Hz (standard for servos)
 }
 
 // the loop routine runs over and over again forever:
@@ -60,55 +47,45 @@ void loop() {
     //Serial.println(on_off);
   }
 
+  if (on_off == 0)
+  {
+    tiltServo.write(60); 
+  }
+
   if (on_off == 1)
   {
-    pos =pos+pos_mov;
-    pos_pan = pos_pan+pos_mov_tilt;
-    myservo.write(pos);
-    myservo2.write(pos);
-    myservo_tilt.write(pos_pan);
-    myservo_pan.write(pos_pan);
-    torsoServo.write(pos_torso);
-    adafruitServoPos += pos_mov_adafruit;
+    pos_tilt = pos_tilt + pos_tilt_mov;
+    pos_arm = pos_arm + pos_arm_mov;
+    tiltServo.write(pos_tilt);
+    rightArmServo.write(pos_arm);
 
-    // Serial.println(pos_pan);
-    // myservo3.write(pos_pan);
-    if (pos < 10 || pos > 180) 
-    {
-      pos_mov = -pos_mov;
+    if (pos_arm < pos_arm_min || pos_arm > pos_arm_max) {
+      pos_arm_mov = -pos_arm_mov; 
     }
-    if (pos_pan < 80 || pos_pan > 110) 
-    {
-      pos_mov_tilt = -pos_mov_tilt;
+    if (pos_tilt < pos_tilt_min || pos_tilt > pos_tilt_max) {
+      pos_tilt_mov = -pos_tilt_mov; 
     }
-    if (pos_torso < 89 || pos_torso > 97)
-    {
-      pos_mov_torso = -pos_mov_torso;
-    }
+  
+  }
+
+  if (on_off == 2)
+  {
+    tiltServo.write(60); 
   }
 
 
-  else if (on_off == 3)
+  if (on_off == 3)
   {
-    pos = pos+pos_mov;
-    myservo.write(pos);
-    myservo2.write(pos);
-    myservo_tilt.write(pos_pan);
-    myservo_pan.write(pos_pan);
-    torsoServo.write(pos_torso);
-    // myservo3.write(pos_pan);
+    pos_tilt = pos_tilt + pos_tilt_mov;
+    pos_arm = pos_arm + pos_arm_mov;
+    tiltServo.write(pos_tilt);
+    rightArmServo.write(pos_arm);
 
-    if (pos < 10 || pos > 180) 
-    {
-      pos_mov = -pos_mov;
+    if (pos_arm < pos_arm_min || pos_arm > pos_arm_max) {
+      pos_arm_mov = -pos_arm_mov; 
     }
-    if (pos_pan < 10 || pos_pan > 40) 
-    {
-      pos_mov_tilt = -pos_mov_tilt;
-    }
-    if (pos_torso < 85 || pos_torso > 95)
-    {
-      pos_mov_torso = -pos_mov_torso;
+    if (pos_tilt < pos_tilt_min || pos_tilt > pos_tilt_max) {
+      pos_tilt_mov = -pos_tilt_mov; 
     }
   }
 
@@ -116,6 +93,6 @@ void loop() {
 
 
   // wait for 30 milliseconds to see the dimming effect
-  delay(15);
+  delay(45);
   
 }
---
+++ full_motion.py
@@ -0,0 +1,70 @@
+from pynput import keyboard
+from recorder import recorder
+from player import player
+from serial import Serial
+from threading import Thread
+import sounddevice as sd
+from whisper_live import transcription
+from sentimental_analysis import analyze_sentiment, score_threshold
+import random
+import os
+from pydub import AudioSegment
+
+arduinoComPort = "/dev/ttyACM0"
+baudRate = 9600
+serialPort = Serial(arduinoComPort, baudRate, timeout=5)
+
+
+def convert_m4a_to_wav(input_file, output_file):
+    audio = AudioSegment.from_file(input_file, format="m4a")
+    audio.export(output_file, format="wav")
+
+
+class listener:
+    def __init__(self, recorder, player):
+        serialPort.flushInput()
+
+        self.recorder = recorder
+        self.player = player
+
+    def record(self):
+
+        while True:
+            serialPort.flushInput()
+            lineOfData = serialPort.readline().decode()
+            # print(lineOfData)
+            try:
+                if int(lineOfData) == 0:
+                    pass
+                elif int(lineOfData) == 1:
+                    path = f"recordings/initial_statement"
+                    file = random.choice(os.listdir(path))
+                    convert_m4a_to_wav(path + "/" + file, "output.wav")
+                    play_music = player("output.wav")
+                    play_music.run()
+                elif int(lineOfData) == 2:
+                    self.recorder.start()
+                elif int(lineOfData) == 3 and self.recorder.recording:
+                    self.recorder.stop()
+                    transcribed_text = transcription("mic")
+                    print(transcribed_text)
+                    score = analyze_sentiment(transcribed_text)
+                    path = f"recordings/{score_threshold(score)}"
+                    file = random.choice(os.listdir(path))
+                    convert_m4a_to_wav(path + "/" + file, "output.wav")
+                    play_music = player("output.wav")
+                    play_music.run()
+
+            except ValueError:
+                continue
+
+
+if __name__ == "__main__":
+
+    r = recorder("mic.wav")
+    p = player("mic.wav")
+    listener_class = listener(r, p)
+
+    l = Thread(target=listener_class.record)
+    l.start()  # keyboard listener is a thread so we start it here
+    l.join()  # wait for the tread to terminate so the program doesn't instantly close
